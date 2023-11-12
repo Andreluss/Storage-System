c9fec3e7372dc1ad4e255c85a8caddb862cf1ced
Andrzej Jabłoński <andrzoff@gmail.com>
2023-11-12 10:36:32 +0100

    storagesystemimpl

---
+++ src/cp2023/solution/StorageSystemImpl.java
@@ -0,0 +1,16 @@
+package cp2023.solution;
+
+import cp2023.base.ComponentTransfer;
+import cp2023.base.StorageSystem;
+import cp2023.exceptions.TransferException;
+
+public class StorageSystemImpl implements StorageSystem {
+    /**
+     * @param transfer
+     * @throws TransferException
+     */
+    @Override
+    public void execute(ComponentTransfer transfer) throws TransferException {
+
+    }
+}
